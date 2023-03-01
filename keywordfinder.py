b9f4bd0a3f9cd9eb0def559997dc0b2bd309b330
Minh Giang <giamc-23@rhodes.edu>
2023-02-28 18:35:48 -0600

    Created List format where [month, day, year, event

--- keywordfinder.py
+++ keywordfinder.py
@@ -7,25 +7,26 @@ date_pattern = "[0-9]{1,2}\\S[0-9]{1,2}\\S[0-9]{2,4}"
 date_pattern2 = "[0-9]{1,2}\\/[0-9]{1,2}"
 date_pattern3 = "(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May?|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\\s*[0-9]{1,2}(?:th|st|nd|rd)?\\s*,\s*[0-9]{2,4}"
 myset = {date_pattern, date_pattern2, date_pattern3}
-days = "Monday|Mon|Tuesday|Tue|Wednesday|Wed|Thursday|Thur|Friday|Fri|Saturday|Sat|Sunday|Sun"
+days = "Monday|Mon|Tuesday|Tue|Tues|Wednesday|Wed|Thursday|Thur|Thurs|Friday|Fri|Saturday|Sat|Sunday|Sun"
+simpledays = "MWF|TR|TTh|TT|MW|WF"
 
 
 
 
+def findclasstimes(line):
+    timepattern = "((1[0-2]|0?[1-9]):([0-5][0-9])\s*?([AaPp][Mm])?)"
+    match1 = re.search(r"[Oo]ffice\s[Hh]ours", line)
+    match = re.search(timepattern, line)
+    if match:
+        print(re.findall(timepattern, line))
+        print("Match found!") 
+        print(line)
+    
 def extract_office_hours(text):
     office_hours_regex = re.compile(f"({days})[.]*[\s-]*(and|to)?[\s-]*({days})?")
     match = office_hours_regex.search(text)
     if match:
-        return match.group()
-
-def findclasstimes(line):
-    timepattern = "((1[0-2]|0?[1-9]):([0-5][0-9])\s*?([AaPp][Mm]))"
-    match = re.search(r"[Oo]ffice\s[Hh]ours", line)
-    if match:
-        print("Match found!")
-        print(line)
-    
-    
+        return match.group()    
 
   
 def testre():
@@ -35,30 +36,42 @@ def testre():
     print(re.findall(date_pattern3, ' * February 16, 2023  – First Essay Due  '))
     print(re.findall(date_pattern, 'youre 10-12-2012 and 1/20/2012 and 2.19.2022'))
     
-
+def createEvent(date,currevent):
+    
+    date_array = date.replace(",", " ").split(" ")
+    while "" in date_array:
+        date_array.remove("")
+    for i in range(3):
+        currevent[i] = date_array[i]
+    print(currevent)
 
 def main():
+    alldates = []
+    dandEvents = []
     reader = PdfReader("music.pdf")
     number_of_pages = len(reader.pages)
-    page = reader.pages[0]
-    text = page.extract_text()
-    print(text)
-    text_split = text.splitlines()
-
-    for x in text_split:
-        findclasstimes(x)
-
-    print(extract_office_hours("Mon. and Wed"))
-    #print(wordlist)
-
-    #testre()
-
-    #alldates = []
-    #for x in text_split:
-    #    for y in myset:
-    #        if len(re.findall(y,x)) != 0:
-    #            alldates.append(re.findall(y,x))
-
-    #print(alldates)
+    
+    for i in range(number_of_pages):
+        page = reader.pages[i]
+        text = page.extract_text()
+        text_split = text.splitlines()
+        
+        for x in text_split:
+            #findclasstimes(x)
+            for y in myset:
+                match = re.search(y, x)
+                #if match:
+                currevent = ["", "", "", ""]
+                if len(re.findall(y,x)) != 0:
+                    alldates.append(re.findall(y,x)[0])
+                    onlyevent = x
+                    onlyevent = onlyevent.replace(re.findall(y,x)[0], "")
+                    currevent[3] = onlyevent
+                    #print(currevent)
+                    #print(onlyevent)
+                    createEvent(re.findall(y,x)[0],currevent)
+    
+        
+    
 
 main()
